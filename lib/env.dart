afb56a0a6c97c4d14ccd00ef0a59dba358f9f93c
Anton Adamansky <adamansky@gmail.com>
2019-11-12 20:29:14 +0700

    * Removed injector

--- lib/env.dart
+++ lib/env.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:injector/injector.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'log/loggable.dart';
@@ -21,24 +20,15 @@ abstract class Env<W extends Widget> with Loggable<Env> {
     ]);
   }
 
-  Injector injector = Injector.appInstance;
   Directory dataDirectory;
   Directory tempDirectory;
 
-  T getDependency<T>({String dependencyName = ""}) =>
-      injector.getDependency<T>(dependencyName: dependencyName);
-
   Future<W> open() async {
-    injector = Injector.appInstance;
     await initLogging();
-    injector.registerDependency<Env>((_) => this);
-
     dataDirectory = await getApplicationDocumentsDirectory();
     tempDirectory = await getTemporaryDirectory();
-
     await Env.resetOrientation();
-
-    final rootWidget = await openImpl(injector);
+    final rootWidget = await openImpl();
     log.info('Application initialized');
     return rootWidget;
   }
@@ -50,5 +40,5 @@ abstract class Env<W extends Widget> with Loggable<Env> {
   }
 
   @protected
-  Future<W> openImpl(Injector injector);
+  Future<W> openImpl();
 }
--- lib/env_stegos.dart
+++ lib/env_stegos.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:injector/injector.dart';
 import 'package:provider/provider.dart';
 import 'package:stegos_wallet/env.dart';
 import 'package:stegos_wallet/store/store.dart';
@@ -7,12 +6,10 @@ import 'package:stegos_wallet/widgets/lifecycle_watcher_widget.dart';
 
 class StegosEnv extends Env<Widget> {
   @override
-  Future<Widget> openImpl(Injector injector) async {
+  Future<Widget> openImpl() async {
     final rootStore = StegosStore(this);
-    injector.registerSingleton<StegosStore>((_) => rootStore);
     return MultiProvider(
       providers: [
-        Provider<Injector>.value(value: injector),
         Provider<StegosEnv>.value(value: this),
         Provider<StegosStore>.value(value: rootStore),
       ],
