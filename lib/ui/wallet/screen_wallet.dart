7df3eb29854a1f8b5d5338620c20351e8ec1ca14
Anton Adamansky <adamansky@gmail.com>
2019-11-18 19:12:59 +0700

    * Screen wallet

---
+++ lib/ui/wallet/screen_wallet.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/widgets.dart';
+
+/// Main wallet screen with integrated TabBar.
+///
+class WalletScreen extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _WalletScreenState();
+}
+
+class _WalletScreenState extends State<WalletScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    // TODO: implement build
+    return null;
+  }
+
+}
