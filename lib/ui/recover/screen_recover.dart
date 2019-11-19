1f3c5ef0b6bfd4f6598af637879be74e4c09f22d
anton <menshickov@softmotions.com>
2019-11-19 16:26:57 +0700

    backup screen design

--- lib/ui/recover/screen_recover.dart
+++ lib/ui/recover/screen_recover.dart
@@ -12,41 +12,60 @@ class RecoverScreen extends StatefulWidget {
 
 class _RecoverScreenState extends State<RecoverScreen> {
   final _store = RecoverScreenStore(24);
+  static const _iconBackImage = AssetImage('assets/images/arrow_back.png');
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBarWidget(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () => Navigator.pop(context, false),
-          ),
-          title: const Text('Restore account'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Padding(
-              padding: StegosThemes.defaultPadding,
-              child: Text(
-                'In order to restore your existing account, please fill all '
-                'words from the recovery phrase in correct order.',
-                textAlign: TextAlign.center,
-                style: StegosThemes.defaultCaptionTextStyle,
+  Widget build(BuildContext context) => Theme(
+        data: StegosThemes.backupTheme,
+        child: Scaffold(
+          appBar: AppBarWidget(
+            centerTitle: false,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            leading: IconButton(
+              icon: SizedBox(
+                width: 24,
+                height: 24,
+                child: Image(image: _iconBackImage),
               ),
+              onPressed: () => Navigator.pop(context, false),
             ),
-            Expanded(
-              child: SingleChildScrollView(child: _buildForm()),
-            ),
-            SizedBox(
-                width: double.infinity,
-                child: Padding(padding: StegosThemes.defaultPadding, child: _buildRestoreButton()))
-          ],
+            title: const Text('Account back up'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Padding(
+                padding: StegosThemes.defaultPadding,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Please white down the phase in case to restore your account',
+                      textAlign: TextAlign.center,
+                      style: StegosThemes.defaultCaptionTextStyle,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'The field is case sensitive',
+                      textAlign: TextAlign.center,
+                      style: StegosThemes.defaultSubCaptionTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(child: _buildForm()),
+              ),
+              SizedBox(width: double.infinity, height: 50, child: _buildRestoreButton())
+            ],
+          ),
         ),
       );
 
   Widget _buildRestoreButton() => Observer(
       builder: (_) => RaisedButton(
+            elevation: 8,
+            disabledElevation: 8,
             onPressed: _store.valid ? _onRestore : null,
-            child: const Text('RESTORE'),
+            child: const Text('VERIFY'),
           ));
 
   Widget _buildForm() => Form(
@@ -63,10 +82,22 @@ class _RecoverScreenState extends State<RecoverScreen> {
         ),
       );
 
-  Widget _buildTextEntry(int idx, String text) => TextField(
-        onChanged: (val) => _store.setKey(idx, val),
-        decoration:
-            InputDecoration(icon: Container(width: 24, child: Text('${idx + 1}.')), hintText: ''),
+  Widget _buildTextEntry(int idx, String text) => Stack(
+        alignment: Alignment.centerLeft,
+        children: <Widget>[
+          Builder(
+              builder: (context) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 13.0, vertical: 6.0),
+                    child: Text(
+                      '${idx + 1}.',
+                      style: StegosThemes.defaultInputTextStyle,
+                    ),
+                  )),
+          TextField(
+            onChanged: (val) => _store.setKey(idx, val),
+            style: StegosThemes.defaultInputTextStyle,
+          )
+        ],
       );
 
   void _onRestore() {
