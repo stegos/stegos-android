0114557de08c0bb4732e3cdff6c03340d54f57ec
anton <menshickov@softmotions.com>
2019-11-25 16:09:12 +0700

    sortable accounts

--- lib/ui/wallet/wallet/screen_accounts.dart
+++ lib/ui/wallet/wallet/screen_accounts.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:stegos_wallet/ui/themes.dart';
+import 'package:stegos_wallet/widgets/reorderable_list.dart';
 
 import 'account_card.dart';
 
@@ -16,13 +17,18 @@ class AccountsScreenState extends State<AccountsScreen> {
   final SvgPicture accountsIcon = SvgPicture.asset('assets/images/accounts.svg');
 
   final Image qrIcon =
-  Image(image: const AssetImage('assets/images/qr.png'), width: 20, height: 20);
+      Image(image: const AssetImage('assets/images/qr.png'), width: 20, height: 20);
+  List<Account> accountsList = <Account>[
+    Account(name: 'Account 1', balance: 345, qrUrl: null, order: 0),
+    Account(name: 'Account 2', balance: 126, qrUrl: null, order: 1),
+    Account(name: 'Account 3', balance: 0.302, qrUrl: null, order: 2),
+    Account(name: 'Account 4', balance: -11, qrUrl: null, order: 3),
+  ];
 
   bool collapsed = false;
 
-  Container _buildTotalBalanceContainer() =>
-      Container(
-        height: 150,
+  Container _buildTotalBalanceContainer() => Container(
+        height: 130,
         color: StegosColors.backgroundColor,
         child: Stack(children: [
           Container(
@@ -37,41 +43,40 @@ class AccountsScreenState extends State<AccountsScreen> {
                   const Text('Total balance'),
                   const SizedBox(
                     height: 15,
-                    ),
+                  ),
                   Text('0 STG', style: TextStyle(fontSize: 24, color: StegosColors.primaryColor))
                 ],
-                ),
               ),
             ),
-          InkWell(
-            onTap: () {
-              setState(() {
-                collapsed = !collapsed;
-                print(collapsed);
-              });
-            },
-            child: Container(
-              alignment: Alignment.bottomLeft,
-              padding: const EdgeInsets.only(left: 15, bottom: 8),
-              decoration: BoxDecoration(
-                  border: Border(bottom: BorderSide(color: StegosColors.primaryColorDark))),
+          ),
+          Container(
+            alignment: Alignment.bottomLeft,
+//            padding: const EdgeInsets.only(left: 15, bottom: 8),
+            decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: StegosColors.primaryColorDark))),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  collapsed = !collapsed;
+                  print(collapsed);
+                });
+              },
               child: Row(
                 children: <Widget>[
                   accountsIcon,
                   const SizedBox(width: 15),
                   const Text('Accounts'),
                 ],
-                ),
               ),
             ),
+          ),
         ]),
-        );
+      );
 
   void _settingModalBottomSheet(BuildContext context) {
     showModalBottomSheet(
         context: context,
-        builder: (BuildContext bc) =>
-            Container(
+        builder: (BuildContext bc) => Container(
               child: Wrap(
                 crossAxisAlignment: WrapCrossAlignment.center,
                 children: <Widget>[
@@ -79,26 +84,20 @@ class AccountsScreenState extends State<AccountsScreen> {
                     title: const Text(
                       'New account',
                       textAlign: TextAlign.center,
-                      ),
                     ),
+                  ),
                   ListTile(
                       leading: Icon(Icons.restore_page),
                       title: const Text('Recover account'),
-                      onTap: () =>
-                      {
-                      Navigator.pushNamed(context, '/restore')
-                      }),
+                      onTap: () => {Navigator.pushNamed(context, '/restore')}),
                   ListTile(
                     leading: Icon(Icons.add),
                     title: const Text('Create new account'),
-                    onTap: () =>
-                    {
-                    _showMaterialDialog()
-                    },
-                    ),
+                    onTap: () => {_showMaterialDialog()},
+                  ),
                 ],
-                ),
-              ));
+              ),
+            ));
   }
 
   void _showMaterialDialog() {
@@ -113,7 +112,7 @@ class AccountsScreenState extends State<AccountsScreen> {
               onChanged: (text) {
                 accountName = text;
               },
-              ),
+            ),
             actions: <Widget>[
               FlatButton(
                   onPressed: () {
@@ -127,68 +126,63 @@ class AccountsScreenState extends State<AccountsScreen> {
                   _dismissDialog();
                 },
                 child: const Text('CREATE'),
-                )
+              )
             ],
-            );
+          );
         });
   }
 
   void _dismissDialog() => Navigator.pop(context);
+
+  List<Widget> _accountsArray(List<Account> accounts) {
+    final list = accounts
+        .map<AccountCard>((item) => AccountCard(
+              key: ValueKey(item),
+              collapsed: collapsed,
+              order: item.order,
+            ))
+        .toList();
+    return list;
+  }
+
+  Widget _buildCollapsedCards() => ReorderableList(
+      onReorder: (int oldIndex, int newIndex) {
+        // These two lines are workarounds for ReorderableListView
+        if (newIndex > accountsList.length) newIndex = accountsList.length;
+        if (oldIndex < newIndex) newIndex--;
+        setState(() {
+          final Account item = accountsList[oldIndex];
+          accountsList.remove(item);
+          accountsList.insert(newIndex, item);
+          accountsList.asMap().forEach((index, value) => value.order = index);
+        });
+      },
+      padding: EdgeInsets.only(bottom: 80, top: collapsed ? 22 : 15, left: 30, right: 30),
+      children: _accountsArray(accountsList));
+
+  Widget _buildNormalCards() => ListView(
+      padding: EdgeInsets.only(bottom: 80, top: collapsed ? 22 : 15, left: 30, right: 30),
+      children: _accountsArray(accountsList));
+
+  Widget _buildCards() => collapsed ? _buildCollapsedCards() : _buildNormalCards();
+
   @override
-  Widget build(BuildContext context) {
-    final accountsList = <Widget>[
-      AccountCard(
-        account: Account(name: 'Account 1', balance: 345, qrUrl: null),
-        collapsed: collapsed,
-        index: 0,
-        key: const Key('0'),
-        ),
-      SizedBox(height: collapsed ? 8 : 30, key: const  Key('00'),),
-      AccountCard(
-        account: Account(name: 'Account 2', balance: 126, qrUrl: null),
-        collapsed: collapsed,
-        index: 1,
-        key: const Key('1'),
-        ),
-      SizedBox(height: collapsed ? 8 : 30, key: const  Key('11'),),
-      AccountCard(
-        account: Account(name: 'Account 3', balance: 0.302, qrUrl: null),
-        collapsed: collapsed,
-        index: 2,
-        key: const Key('2'),
-        ),
-      SizedBox(height: collapsed ? 8 : 30, key: const  Key('22'),),
-      AccountCard(
-        account: Account(name: 'Account 4', balance: -11, qrUrl: null),
-        collapsed: collapsed,
-        index: 3,
-        key: const Key('3'),
-        ),
-    ];
-    return Theme(
-      data: StegosThemes.accountsTheme,
-      child: Scaffold(
-        body: Stack(
-          alignment: Alignment.topCenter,
-          children: <Widget>[
+  Widget build(BuildContext context) => Theme(
+        data: StegosThemes.accountsTheme,
+        child: Scaffold(
+          body: Stack(alignment: Alignment.topCenter, children: <Widget>[
             _buildTotalBalanceContainer(),
             Container(
-              padding: const EdgeInsets.only(top: 150),
-              child: ReorderableListView(
-                onReorder: (int aInd, int bInd) {},
-                padding: const EdgeInsets.only(bottom: 80, top: 30, left: 30, right: 30),
-                children: accountsList,
-                ),
-              ),
-          ],
-          ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _settingModalBottomSheet(context);
-          },
-          child: Icon(Icons.add),
+                padding: const EdgeInsets.only(top: 130),
+                child: _buildCards())
+
+          ]),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              _settingModalBottomSheet(context);
+            },
+            child: Icon(Icons.add),
           ),
         ),
       );
-  }
 }
