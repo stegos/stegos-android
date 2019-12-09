7412a4dea024e711468b9f7fa4e72991ade6feb3
anton <menshickov@softmotions.com>
2019-12-09 16:14:24 +0700

    added packages for reading and generationg qr codes

---
+++ lib/ui/wallet/qr_reader/qr_reader.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class QrReader extends StatefulWidget {
+  @override
+  _QrReaderState createState() => _QrReaderState();
+}
+
+class _QrReaderState extends State<QrReader> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
