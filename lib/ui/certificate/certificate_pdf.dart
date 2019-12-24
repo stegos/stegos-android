3891ffc1ef909d20a0823be35930eb584bdb96bb
Михаил Остроушенко <ostroushenko@softmotions.com>
2019-12-24 14:48:53 +0700

    pdf certificate mockup

--- lib/ui/certificate/certificate_pdf.dart
+++ lib/ui/certificate/certificate_pdf.dart
@@ -1,100 +1,68 @@
-import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart';
 import 'package:stegos_wallet/services/service_node.dart';
 
-Future<Document> generateDocument(TxStore transaction, TxValidationInfo txvInfo) async {
+Future<Document> generateDocument(
+    TxStore transaction, TxValidationInfo txvInfo) async {
   final Document pdf = Document(title: 'Stegos Certificate', author: 'stegos');
 
   pdf.addPage(Page(
-    build: (Context context) => Column(
-      children: <Widget>[
-        Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          Container(
-              child: Container(
-                  padding: const EdgeInsets.only(top: 15),
-//                  color: StegosColors.splashBackground,
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.only(
-                            left: 15, right: 15),
-                        child:
-                        Row(mainAxisAlignment: MainAxisAlignment
-                            .spaceBetween, children: [
-                          Text(transaction.humanCreationTime,
-                              style: const TextStyle(fontSize: 14)),
-                          Text(transaction.humanStatus,
-                              style: const TextStyle(fontSize: 14))
-                        ]),
-                      ),
-                      Text('${transaction.humanAmount} STG',
-                          style: const TextStyle(fontSize: 32)),
-                    ],
-                  ))),
-        ]),
-        Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.only(left: 10, right: 10),
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Transaction data',
-                    style: TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                _buildTxValue(
-                    'Sender', '${transaction.account.pkey}'),
-                _buildTxValue('Recepient',
-                    '${transaction.certOutput['recipient']}'),
-                _buildTxValue('R-value',
-                    '${transaction.certOutput['rvalue']}'),
-                _buildTxValue('UTXO ID',
-                    '${transaction.certOutput['output_hash']}'),
-                if (txvInfo != null) ...[
-                  Text(
-                    'Transaction verification',
-                    style: TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  Row(
+      build: (Context context) => Column(children: <Widget>[
+            Container(
+                child: Text('Payment Certificate',
+                    style: const TextStyle(fontSize: 32))),
+            Container(
+                margin: const EdgeInsets.only(top: 5),
+                child: Text('Generated on ${transaction.humanCreationTime}',
+                    style: const TextStyle(fontSize: 12))),
+            Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                alignment: Alignment.centerLeft,
+                child: Text('Transaction Data',
+                    style: const TextStyle(fontSize: 24))),
+            _buildTxValue('Sender', '${transaction.account.pkey}'),
+            _buildTxValue(
+                'Recipient', '${transaction.certOutput['recipient']}'),
+            _buildTxValue('R-value', '${transaction.certOutput['rvalue']}'),
+            _buildTxValue(
+                'UTXO ID', '${transaction.certOutput['output_hash']}'),
+            Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                alignment: Alignment.centerLeft,
+                child: Text('Transaction verification',
+                    style: const TextStyle(fontSize: 24))),
+            Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text('Sender: Valid'),
-                      Text('UTXO ID: Valid'),
-                    ],
-                  ),
-                  Row(
-                    children: [
                       Text('Recipient: Valid'),
-                      Text('UTXO Block: #${txvInfo.epoch}'),
-                    ],
-                  ),
-                ]
-              ],
-            ),
-      ],
-    ),
-  ));
+                      Text('UTXO ID: Valid'),
+                    ])),
+            Container(
+                alignment: Alignment.centerRight,
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                child: Text('UTXO Block No: ${txvInfo.epoch}')),
+            Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                    'Amount: ${(txvInfo.amount / 1e6).toStringAsFixed(4)} STG')),
+          ])));
   return pdf;
 }
 
 Widget _buildTxValue(String label, String value) {
   return Container(
     margin: const EdgeInsets.only(bottom: 10),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    child: Row(
       children: <Widget>[
         Text(
-          label,
-          style: TextStyle(
-              fontSize: 12),
+          '${label}: ',
+          style: const TextStyle(fontSize: 12),
         ),
         Text(
           value,
-          style: TextStyle(
-              fontSize: 12),
+          style: const TextStyle(fontSize: 12),
         ),
       ],
     ),
